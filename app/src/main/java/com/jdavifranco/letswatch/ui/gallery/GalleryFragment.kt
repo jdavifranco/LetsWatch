942293e57ffa4a54c24001ad511a6f310f8391c4
jdavifranco <developerjdavi@gmail.com>
2021-08-03 16:22:41 -0300

    "add query parameters"

--- app/src/main/java/com/jdavifranco/letswatch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/jdavifranco/letswatch/ui/gallery/GalleryFragment.kt
@@ -2,52 +2,70 @@ package com.jdavifranco.letswatch.ui.gallery
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TableLayout
-import android.widget.TextView
-import androidx.core.net.toUri
-import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.tabs.TabLayout
 import com.jdavifranco.letswatch.R
-import com.jdavifranco.letswatch.database.Genre
-import com.jdavifranco.letswatch.database.Movie
 import com.jdavifranco.letswatch.databinding.GalleryFragmentBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
+private const val ARG_QUERY = "POPULAR"
 
 class GalleryFragment : Fragment() {
 
-    val viewModel: GalleryViewModel by viewModel()
+    private val viewModel: GalleryViewModel by viewModel()
     private lateinit var binding:GalleryFragmentBinding
+    private val adapter = MoviesAdapter()
+    private var searchJob: Job? = null
+    private var query: String = "POPULAR"
+
+    private fun search(query: String) {
+        // Make sure we cancel the previous job before creating a new one
+        searchJob?.cancel()
+        searchJob = lifecycleScope.launch {
+            viewModel.searchMovie(query).collect {
+                adapter.submitData(it)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            query = it.getString(ARG_QUERY)?:"POPULAR"
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = DataBindingUtil.inflate(inflater, R.layout.gallery_fragment, container, false)
         binding.lifecycleOwner =this
-        val moviesAdapter = MoviesAdapter()
-        binding.rvMovies.adapter = moviesAdapter
-        viewModel.movies.observe(viewLifecycleOwner, Observer {
-            Log.e("movies size", "${it.size}")
-            moviesAdapter.submitList(it)
-        })
-
-        moviesAdapter.rvPosition.observe(viewLifecycleOwner, Observer {
-            Log.e("position", "$it")
-            Log.e("posMUlt", "$it % 20 = ${it%20}")
-            if(it%20==0){
-                viewModel.getNextPage()
-            }
-        })
-
+        search(query)
+        binding.rvMovies.adapter = adapter
+        binding.rvMovies.setHasFixedSize(true)
         return binding.root
     }
 
+    companion object{
+
+        @JvmStatic
+        fun newInstance(param1: String) =
+            GalleryFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_QUERY, param1)
+                }
+            }
+    }
 
 }
