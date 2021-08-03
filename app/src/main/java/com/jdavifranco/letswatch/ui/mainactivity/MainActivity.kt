ba9e03cfe1beec504e2f2b2c9d4e879f41f6aa5d
jdavifranco <developerjdavi@gmail.com>
2021-08-03 16:23:54 -0300

    "Add Tablayout with ViewPager"

--- app/src/main/java/com/jdavifranco/letswatch/ui/mainactivity/MainActivity.kt
+++ app/src/main/java/com/jdavifranco/letswatch/ui/mainactivity/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TableLayout
 import androidx.lifecycle.Observer
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.jdavifranco.letswatch.R
 import com.jdavifranco.letswatch.ui.gallery.GalleryViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -13,18 +15,21 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
 
     val viewModel: MainActivityViewModel by viewModel()
-
+    private lateinit var tabPagerAdapter:TabPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tabLayout:TabLayout = findViewById(R.id.tabs)
-
+        supportActionBar?.elevation = 0F
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
         viewModel.genres.observe(this, Observer {
-            tabLayout.addTab(tabLayout.newTab().setText("Popular"));
-            for(item in it){
-                tabLayout.addTab(tabLayout.newTab().setText(item.name));
-            }
+            tabPagerAdapter = TabPagerAdapter(this, it)
+            viewPager.adapter = tabPagerAdapter
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+               tab.text = it.get(position).name
+            }.attach()
         })
+
     }
 
 }
