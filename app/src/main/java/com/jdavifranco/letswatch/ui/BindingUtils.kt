d22f0ace561140e56980d28038a30a4115fe160d
jdavifranco <developerjdavi@gmail.com>
2021-08-04 18:01:21 -0300

    "add details binding Adapters"

--- app/src/main/java/com/jdavifranco/letswatch/ui/BindingUtils.kt
+++ app/src/main/java/com/jdavifranco/letswatch/ui/BindingUtils.kt
@@ -1,5 +1,6 @@
 package com.jdavifranco.letswatch.ui
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.net.toUri
@@ -12,8 +13,8 @@ import com.jdavifranco.letswatch.database.Movie
 
 
 @BindingAdapter("voteAverage")
-fun bindMovieVote(textView: TextView, movie: Movie){
-    textView.text = movie.voteAverage.toString()
+fun bindMovieVote(textView: TextView, movie: Movie?){
+    movie?.let { textView.text = it.voteAverage.toString()}
 }
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -26,5 +27,38 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
                     .placeholder(R.drawable.loading_animation)
                     .error(R.drawable.ic_broken_image))
             .into(imgView)
+    }
+}
+
+
+@BindingAdapter("yearAndRuntime")
+fun bindYearRuntime(textView: TextView, movie: Movie?){
+    movie?.let {
+        val year = movie.date.subSequence(0, 4)
+        val runtime = if (movie.detalhes == null) null else movie.detalhes?.runtime
+        if (runtime == null) {
+            textView.text = year
+        } else {
+            textView.text = "$year - $runtime"
+        }
+    }
+}
+
+@BindingAdapter("bindOverview")
+fun bindOverview(textView: TextView, movie:Movie?){
+    movie?.let {
+        val overview = movie.detalhes?.overview
+        if (overview != null) {
+            textView.text = "Overview: $overview"
+        } else {
+            textView.text = ""
+        }
+    }
+}
+
+@BindingAdapter("bindGenres")
+fun bindGenres(textView: TextView, movie:Movie?){
+    movie?.let {
+        textView.text = movie.detalhes?.genres
     }
 }
