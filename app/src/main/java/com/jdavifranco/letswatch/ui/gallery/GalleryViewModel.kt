07ac0625255bc98b2cc75bd30318d6939bbf550f
jdavifranco <20tres69Cinco>
2022-01-30 20:25:45 -0300

    integrate ResponseState in GalleryViewModel

--- app/src/main/java/com/jdavifranco/letswatch/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/jdavifranco/letswatch/ui/gallery/GalleryViewModel.kt
@@ -1,24 +1,48 @@
 package com.jdavifranco.letswatch.ui.gallery
 
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.jdavifranco.letswatch.repository.Repository
 import com.jdavifranco.letswatch.domain.model.Movie
+import com.jdavifranco.letswatch.ui.utils.ResponseState
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class GalleryViewModel(private val repository: Repository) : ViewModel() {
+    private var _responseState = MutableLiveData<ResponseState<Flow<PagingData<Movie>>>>()
+    val responseState:LiveData<ResponseState<Flow<PagingData<Movie>>>> get() = _responseState
+
     private var currentQueryValue: String? = null
     private var currentSearchResult: Flow<PagingData<Movie>>? = null
 
-    fun searchMovies(queryString: String): Flow<PagingData<Movie>> {
+    fun fetchMovies(queryString: String){
+        _responseState.postValue(ResponseState.Loading)
+        try {
+            _responseState.postValue(
+                ResponseState.Success(
+                    result = searchMovies(queryString)
+                ))
+        }catch (e:Exception){
+            _responseState.postValue(ResponseState.Error(e))
+        }
+
+    }
+
+    private fun searchMovies(queryString: String):Flow<PagingData<Movie>> {
         val lastResult = currentSearchResult
         if (queryString == currentQueryValue && lastResult != null) {
             return lastResult
         }
         currentQueryValue = queryString
+
         val newResult: Flow<PagingData<Movie>> = repository.getMoviesStream(queryString)
             .cachedIn(viewModelScope)
         currentSearchResult = newResult
