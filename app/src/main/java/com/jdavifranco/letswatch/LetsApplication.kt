e3f51a6f5e49b0803604d8f43be9e6f744dcc640
jdavifranco <developerjdavi@gmail.com>
2022-02-01 01:07:58 -0300

    add use cases dependency injection

--- app/src/main/java/com/jdavifranco/letswatch/LetsApplication.kt
+++ app/src/main/java/com/jdavifranco/letswatch/LetsApplication.kt
@@ -16,6 +16,7 @@ class LetsApplication: Application() {
             modules(
                 networkModule,
                 repositoryModule,
+                useCaseModule,
                 viewModelModule
             )
         }
