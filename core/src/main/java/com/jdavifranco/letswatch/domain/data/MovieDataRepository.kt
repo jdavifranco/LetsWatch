106bc737fabca4ea6d76caab3759e096a0665a3a
jdavifranco <developerjdavi@gmail.com>
2022-01-31 23:49:19 -0300

    add core module and domain package

--- core/src/main/java/com/jdavifranco/letswatch/domain/data/MovieDataRepository.kt
+++ core/src/main/java/com/jdavifranco/letswatch/domain/data/MovieDataRepository.kt
@@ -1,13 +1,11 @@
-package com.jdavifranco.letswatch.domain.datarepository
+package com.jdavifranco.letswatch.domain.data
 
-import androidx.paging.PagingData
 import com.jdavifranco.letswatch.domain.model.Details
 import com.jdavifranco.letswatch.domain.model.Genre
 import com.jdavifranco.letswatch.domain.model.Movie
-import kotlinx.coroutines.flow.Flow
 
 interface MovieDataRepository {
-    fun getMovieStream(query:String): Flow<PagingData<Movie>>
-    suspend fun getMovieDetails(id:Long):Details
+    suspend fun getMovieListByGenre(query:String, pagePosition:Int): List<Movie>
+    suspend fun getMovieDetails(id:Long): Details
     suspend fun getMovieGenreList():List<Genre>
 }
